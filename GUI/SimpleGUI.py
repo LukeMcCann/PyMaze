a782185038432de3148624bf10c55358ffe4bffc
LukeMcCann <developer-lukemccann@outlook.com>
2019-02-02 22:40:20 +0000

    Simple GUI for weather app

--- GUI/SimpleGUI.py
+++ GUI/SimpleGUI.py
@@ -7,26 +7,50 @@ import tkinter as tk
 from tkinter import Menu, mainloop
 from tkinter import ttk  # themed tk 
 
-### functions
 def _quit():
-    win.quit()
+    win.quit()      
     win.destroy()
-    exit()
-    
-    
-win = tk.Tk()
-win.minsize(500, 500)
-win.title("Python GUI")
+    exit() 
+
+
+win = tk.Tk()         
+win.title("Python Projects")
+win.minsize(200, 200)
 
 menuBar = Menu()
 win.config(menu=menuBar)
 
-fileMenu = Menu(menuBar, tearoff = 0)
-fileMenu.add_command(label = "New")
+# Add menu items
+fileMenu = Menu(menuBar, tearoff=0)
+fileMenu.add_command(label="New")
 fileMenu.add_separator()
-fileMenu.add_command(label= "Exit", command = _quit)
-menuBar.add_cascade(label = "File", menu = fileMenu)
+fileMenu.add_command(label="Exit", command=_quit)  
+menuBar.add_cascade(label="File", menu=fileMenu)
+
+helpMenu = Menu(menuBar, tearoff=0)
+helpMenu.add_command(label="About")
+menuBar.add_cascade(label="Help", menu=helpMenu)
+
+# Tab Control
+tabControl = ttk.Notebook(win)          # Create Tab Control
+
+tab1 = ttk.Frame(tabControl)            # Create a tab 
+tabControl.add(tab1, text='Tab 1')      # Add the tab
+
+tab2 = ttk.Frame(tabControl)            # Add a second tab
+tabControl.add(tab2, text='Tab 2')      # Make second tab visible
+
+tabControl.pack(expand=1, fill="both")  # Pack to make visible
+  
+# container frame for widgets
+weather_conditions_frame = ttk.LabelFrame(
+    tab1, text=' Current Weather Conditions ')
+
+# tkinter grid layout manager
+weather_conditions_frame.grid(column=0, row=0, padx=8, pady=4)
+ttk.Label(weather_conditions_frame, 
+          text="Location:").grid(column=0, row=0, sticky='W')
 
 
-
-mainloop()
+# App Start
+win.mainloop()
