e2e3234a7d28dd0efa4dfc14de302523ee5d70ec
LukeMcCann <developer-lukemccann@outlook.com>
2019-02-02 22:52:10 +0000

    Adds textbox widgets to interface

--- GUI/SimpleGUI.py
+++ GUI/SimpleGUI.py
@@ -53,6 +53,7 @@ ttk.Label(weather_conditions_frame,
           text="Location:").grid(column=0, row=0, sticky='W')
 
 
+# add a combobox - location selection
 city = tk.StringVar()
 citySelected = ttk.Combobox(weather_conditions_frame, width=15, textvar = city)
 
@@ -61,5 +62,57 @@ citySelected['values'] = ('Leeds', 'Manchester', 'London', 'Bristol', 'Birmingha
 citySelected.grid(column=1, row=0)
 citySelected.current(0)
 
+# set max combobox length to the length of the longest value
+max_width  = max([len(x)for x in citySelected['values']])
+new_width = max_width
+citySelected.config(width=new_width)
+
+ENTRY_WIDTH = max_width + 3 
+
+# Label and textBox widgets
+ttk.Label(weather_conditions_frame, text="Last Updated:").grid(column=0, row=1, sticky='W')
+updated = tk.StringVar()
+updatedEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=updated, state='readonly')
+updatedEntry.grid(column=1, row=1, sticky='W')
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="Weather:").grid(column=0, row=2, sticky='W')               # <== increment row for each
+weather = tk.StringVar()
+weatherEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=weather, state='readonly')
+weatherEntry.grid(column=1, row=2, sticky='W')                                  # <== increment row for each
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="Temperature:").grid(column=0, row=3, sticky='W')
+temp = tk.StringVar()
+tempEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=temp, state='readonly')
+tempEntry.grid(column=1, row=3, sticky='W')
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="Dewpoint:").grid(column=0, row=4, sticky='W')
+dew = tk.StringVar()
+dewEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=dew, state='readonly')
+dewEntry.grid(column=1, row=4, sticky='W')
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="Relative Humidity:").grid(column=0, row=5, sticky='W')
+rel_humi = tk.StringVar()
+rel_humiEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=rel_humi, state='readonly')
+rel_humiEntry.grid(column=1, row=5, sticky='W')
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="Wind:").grid(column=0, row=6, sticky='W')
+wind = tk.StringVar()
+windEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=wind, state='readonly')
+windEntry.grid(column=1, row=6, sticky='W')
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="Visibility:").grid(column=0, row=7, sticky='W')
+visi = tk.StringVar()
+visiEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=visi, state='readonly')
+visiEntry.grid(column=1, row=7, sticky='W')
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="MSL Pressure:").grid(column=0, row=8, sticky='W')
+msl = tk.StringVar()
+mslEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=msl, state='readonly')
+mslEntry.grid(column=1, row=8, sticky='W')
+#---------------------------------------------
+ttk.Label(weather_conditions_frame, text="Altimeter:").grid(column=0, row=9, sticky='W')
+alti = tk.StringVar()
+altiEntry = ttk.Entry(weather_conditions_frame, width=ENTRY_WIDTH, textvariable=alti, state='readonly')
+altiEntry.grid(column=1, row=9, sticky='W')
 # App Start
 win.mainloop()
